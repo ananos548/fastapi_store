73591ca929faee0d766ef9a849dde31f5c5bce78
ananos548 <inabsm@mail.ru>
2023-05-19 18:40:53 +0300

    add search

--- src/store/router_product.py
+++ src/store/router_product.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select, insert, orm, update
+from sqlalchemy import select, insert, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
@@ -31,4 +31,19 @@ async def get_products_by_category(category_name: str,
                                    session: AsyncSession = Depends(get_async_session)):
     query = select(Product).join(Category).where(Category.title == category_name)
     result = await session.execute(query)
+    return result.scalars().all()
+
+
+@router.get('/product/{id}')
+async def product_detail(id: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(Product).where(Product.id == id)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+@router.get('/search')
+async def search(query: str, session: AsyncSession = Depends(get_async_session)):
+    query_search = select(Product).filter(or_(Product.name.contains(query), Product.description.contains(query)))
+    print(query_search)
+    result = await session.execute(query_search)
     return result.scalars().all()
