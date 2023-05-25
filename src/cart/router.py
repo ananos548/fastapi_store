112ca07b167634d724e930aa8aa7d84bef2dd649
ananos548 <inabsm@mail.ru>
2023-05-26 02:38:55 +0300

    add clear cart

--- src/cart/router.py
+++ src/cart/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi import Request
-from sqlalchemy import select, orm, insert, delete
+from sqlalchemy import select, orm, insert, delete, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import join
 from sqlalchemy.sql.operators import and_
@@ -35,11 +35,40 @@ async def add_to_cart(cart: CartSchema, user: User = Depends(current_user),
 
 
 @router.delete('/cart/{product_id}')
-async def remove_from_cart(product_id: int, user: User = Depends(current_user),
+async def remove_from_cart(cart: CartSchema, user: User = Depends(current_user),
                            session: AsyncSession = Depends(get_async_session)):
-    delete_stmt = delete(Cart).where(and_(Cart.user_id == user.id, Cart.product_id == product_id))
+    delete_stmt = delete(Cart).where(and_(Cart.user_id == user.id, Cart.product_id == cart.product_id))
+
+    existing_cart = await session.execute(
+        select(Cart).where(and_(Cart.user_id == user.id, Cart.product_id == cart.product_id))
+    )
+    cart_scalar = existing_cart.scalar_one_or_none()
+
+    if cart_scalar:
+        current_quantity = cart_scalar.quantity
+        if cart.quantity == 0:
+            await session.execute(delete_stmt)
+        if cart.quantity >= current_quantity:
+            await session.execute(delete_stmt)
+        else:
+            update_stmt = update(Cart).where(and_(Cart.user_id == user.id, Cart.product_id == cart.product_id)).values(
+                quantity=current_quantity - cart.quantity
+            )
+            await session.execute(update_stmt)
+
+        await session.commit()
+        return {"status": "success"}
+
+
+@router.post('/cart/clear')
+async def cart_clear(user: User = Depends(current_user),
+                     session: AsyncSession = Depends(get_async_session)):
+    delete_stmt = delete(Cart).where(Cart.user_id == user.id)
+    print(delete_stmt)
     await session.execute(delete_stmt)
     await session.commit()
+    return {"status": "success"}
+
 
 
 @router.get('/cart/checkout')
@@ -51,11 +80,9 @@ async def checkout_cart(user: User = Depends(current_user),
     ).where(Cart.user_id == user.id)
     result = await session.execute(query)
     products = result.scalars().all()
-
     # Получение количества продуктов в корзине
     cart_quantity = await session.execute(select(Cart).where(Cart.user_id == user.id))
     carts = cart_quantity.scalars().all()
-
     # Формирование данных о продуктах
     product_data = []
     for product, cart in zip(products, carts):
@@ -65,8 +92,6 @@ async def checkout_cart(user: User = Depends(current_user),
             'price': product.price,
             'quantity': cart.quantity
         })
-
     # Вычисление общей стоимости
     total_price = sum(cart.quantity * product.price for cart, product in zip(carts, products))
-
     return {'product_data': product_data, 'total_price': total_price}
